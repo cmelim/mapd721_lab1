11bb14a94284411a1ff62fec6265382bec5beef1
Camilo Meli <camilo.meli@gmail.com>
2024-02-01 09:43:29 -0500

    Initial Commit ADD .gitignore and

---
+++ app/src/main/java/ca/centennial/mapd721/MainActivity.kt
@@ -0,0 +1,46 @@
+package ca.centennial.mapd721
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ca.centennial.mapd721.ui.theme.CamiloMeliDataStoreTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CamiloMeliDataStoreTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CamiloMeliDataStoreTheme {
+        Greeting("Android")
+    }
+}
